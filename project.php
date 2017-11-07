e8da626f9a02cbf9bb46858e9f31dd015409760f
BH210-07\ipd <hossein_ghaffary@yahoo.ca>
2017-11-07 13:27:18 -0500

    11/7/2017

---
+++ project.php
@@ -0,0 +1,75 @@
+
+
+
+
+
+<?php
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+session_start();
+
+require_once 'vendor/autoload.php';
+
+DB::$dbName = 'cp4809_onlineExam';
+DB::$user = 'cp4809_onlineExa';
+DB::$encoding = 'utf8';
+DB::$password = 'uhh$C6MhNb;F';
+
+DB::$error_handler = 'sql_error_handler';
+DB::$nonsql_error_handler = 'nonsql_error_handler';
+
+function sql_error_handler($params) {
+    global $app, $log;
+    $log->err("SQL Error: " . $params['error']);
+    $log->err(" in query: " . $params['query']);
+    http_response_code(500);
+    $app->render('error_internal.html.twig');
+    die;
+}
+
+function nonsql_error_handler($params) {
+    global $app, $log;
+    $log->err("SQL Error: " . $params['error']);
+    http_response_code(500);
+    $app->render('error_internal.html.twig');
+    die;
+}
+//-------------------------------------------------------------
+// Slim creation and setup
+$app = new \Slim\Slim(array(
+    'view' => new \Slim\Views\Twig()
+        ));
+
+$view = $app->view();
+$view->parserOptions = array(
+    'debug' => true,
+    'cache' => dirname(__FILE__) . '/cache'
+);
+$view->setTemplatesDirectory(dirname(__FILE__) . '/templates');
+
+// create a log channel
+$log = new Logger('main');
+$log->pushHandler(new StreamHandler('logs/everything.log', Logger::DEBUG));
+$log->pushHandler(new StreamHandler('logs/errors.log', Logger::ERROR));
+
+
+if (!isset($_SESSION['user'])) {
+    $_SESSION['user'] = array();
+}
+
+// URL/event handlers go here
+$app->get('/', function() use ($app) {
+    echo "This is Teacher's Project";
+});
+
+require_once 'account.php';
+
+require_once 'admin.php';
+
+require_once 'cart.php';
+
+
+
+$app->run();
